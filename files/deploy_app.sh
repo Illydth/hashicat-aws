418366caa893fde37a465e05851dc865f9cc35ff
Douglas Wagner <illydth@gmail.com>
2021-06-10 15:37:01 -0500

    Update deploy_app.sh
    Added Text

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -11,7 +11,7 @@ cat << EOM > /var/www/html/index.html
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
   <center><h2>Meow World!</h2></center>
-  Welcome to ${PREFIX}'s app. Replace this text with your own.
+  Welcome to ${PREFIX}'s app. A Marketing Slogan i'm not going to put time into coming up with.
   <!-- END -->
 
   </div>
